3678f456e514211ac2fcde3a3c4ff7e31f817d91
matias22lucena <matias22lucena@gmail.com>
2026-07-06 15:13:06 -0300

    se creo la ruta para editar clase

--- src/routes/clases.routes.js
+++ src/routes/clases.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   crearClase,
   obtenerClases,
+  editarClase,
   eliminarClase,
 } from "../controllers/clases.controllers.js";
 
@@ -9,5 +10,7 @@ const router = Router();
 
 router.post("/", crearClase);
 router.get("/", obtenerClases);
+router.put("/:id", editarClase);
 router.delete("/:id", eliminarClase);
+
 export default router;
